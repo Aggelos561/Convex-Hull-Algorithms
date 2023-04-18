f92508b6727fbbcb579c7eeedc1b26cf8cc0a279
Aggelos <angelos561@hotmail.com>
2023-04-18 20:45:59 +0300

    continued beneath beyond algorithm

--- beneathBeyond.py
+++ beneathBeyond.py
@@ -43,6 +43,62 @@ def find_red_segment(possible_reds, point, convex_points):
     return None
 
 
+def find_crimson_points(red_seg, convex_segments, convex_points, point):
+    
+    convex = convex_segments
+
+    crimson_points = []
+    all_red_segments = [red_seg]
+
+    found_red = False
+    crimson_counter = 0
+
+    while True:
+
+        for segment in convex:
+
+            if not found_red and segment == red_seg:
+                found_red = True
+                continue
+
+            if found_red:
+
+                red_segment_check = find_red_segment([segment], point, convex_points)
+
+                if red_segment_check is None:
+                    crimson_points.append(segment[crimson_counter])
+                    crimson_counter += 1
+                    convex.reverse()
+                    break
+                
+                else:
+                    all_red_segments.append(red_segment_check)
+
+        if crimson_counter == 2:
+            return all_red_segments, crimson_points
+          
+
+def restructure_convex(convex_segments, convex_points, red_segments, crimson_points, point):
+    
+    for red in set(red_segments):
+        convex_segments.remove(red)
+
+    print(f'CONVEX = {convex_segments}')
+
+    # for segment in convex_segments:
+    #     if segment[0] == crimson_points[0]:
+    #         pass
+
+    convex_points = [segment[0] for segment in convex_segments]
+    convex_points.append(crimson_points[0])
+    convex_points.append(crimson_points[1])
+    convex_points.append(point)
+
+    print(f'convex points = {convex_points}')
+    convex_points = quickHull(convex_points)
+    convex_segments = create_segments(convex_points)
+
+    return convex_points, convex_segments
 
 
 def beneathBeyond(points):
@@ -62,11 +118,15 @@ def beneathBeyond(points):
         possible_reds = find_possible_reds(convex_segments, points_list[k-1])
 
         red_segment = find_red_segment(possible_reds, point, convex_points)
+        
+        all_reds, crimson_points = find_crimson_points(red_segment, convex_segments, convex_points, point)
 
-        print(f'red segment = {red_segment}')
-        break
+        convex_points, convex_segments = restructure_convex(convex_segments, convex_points, all_reds, crimson_points, point)
 
-    return triangle_points
+        print(f'red segments = {all_reds}')
+        print(f'crimson points = {crimson_points}')
+
+    return convex_points
 
 
 
